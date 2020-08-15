f16a87d89a795d620c173f47d7dfe1d32c77611f
László Oláh <olahlaszlo91@gmail.com>
2020-08-15 15:52:45 +0200

    Add chips-search to Tools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
-import {Component} from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {KeyValue} from '@angular/common';
 
-enum Label {
-	JS = 'JavaScript',
-	NODEJS = 'NodeJS',
-	CSS = 'css',
-	SCSS = 'scss',
-	UX = 'UX',
-	IMAGES = 'Images',
-	ANGULAR = 'Angular',
-	HTTP = 'HTTP',
-	TOOL = 'Tool',
+interface Label {
+	[key: string]: string;
 }
 
 interface Link {
 	href: string;
 	name: string;
 	description?: string;
-	labels: Label[];
+	labels: string[];
 }
 
 @Component({
@@ -25,34 +22,134 @@ interface Link {
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	selectable = true;
+	removable = true;
+	separatorKeysCodes: number[] = [ENTER, COMMA];
+
+	@ViewChild('labelInput') labelInput: ElementRef<HTMLInputElement>;
+	@ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+	labelCtrl = new FormControl();
+	labels: Label = {
+		js: 'JavaScript',
+		nodejs: 'NodeJS',
+		css: 'css',
+		scss: 'scss',
+		ux: 'UX',
+		images: 'Images',
+		angular: 'Angular',
+		http: 'HTTP',
+		tool: 'Tool',
+	};
+	filteredLabels: Label = JSON.parse(JSON.stringify(this.labels));
+	selectedLabels: Label = {};
+
 	links: Link[] = [
 		{
 			name: 'Workly', href: 'https://github.com/pshihn/workly',
 			description: 'A really simple way to move a stand-alone function/class to a worker thread.',
-			labels: [Label.JS, Label.NODEJS],
+			labels: [this.labels.js, this.labels.nodejs],
 		},
 		{
 			name: 'requestbin', href: 'https://requestbin.com/',
 			description: 'Get a URL where you can collect HTTP or webhook requests, or connect your accounts to subcribe to events from popular apps. Inspect your data in a human-friendly way or via an API.',
-			labels: [Label.TOOL, Label.HTTP],
+			labels: [this.labels.tool, this.labels.http],
 		},
 		{
 			name: 'Postwoman', href: 'https://postwoman.io/',
-			labels: [Label.TOOL],
+			labels: [this.labels.tool],
 		},
 		{
 			name: 'npkill', href: 'https://npkill.js.org/',
 			description: 'Easily find and remove old and heavy node_modules folders',
-			labels: [Label.TOOL],
+			labels: [this.labels.tool],
 		},
 		{
 			name: 'angryTools', href: 'http://angrytools.com/',
-			description: 'Easily find and remove old and heavy node_modules folders',
-			labels: [Label.TOOL, Label.CSS, Label.SCSS, Label.UX],
+			labels: [this.labels.tool, this.labels.css, this.labels.scss, this.labels.ux],
 		},
 	];
+	filteredLinks: Link[] = JSON.parse(JSON.stringify(this.links));
 
 	openLink(index: number): void {
 		window.open(this.links[index].href, '_blank');
+	}
+
+	constructor() {
+		this.labelCtrl.valueChanges.subscribe((label) => {
+			if (label) {
+				this.filteredLabels = this._filter(label);
+			} else {
+				this.filteredLabels = JSON.parse(JSON.stringify(this.labels));
+			}
+		});
+	}
+
+	add(event: MatChipInputEvent): void {
+		const input = event.input;
+		const value = event.value;
+
+		if ((value || '').trim()) {
+			// Note: new label not allowed
+		}
+
+		if (input) {
+			input.value = '';
+		}
+
+		this.labelCtrl.setValue(null);
+		this.filterLinks();
+
+	}
+
+	remove(label: KeyValue<string, string>): void {
+		delete this.selectedLabels[label.key];
+		this.filterLinks();
+
+	}
+
+	selected(event: MatAutocompleteSelectedEvent): void {
+		this.selectedLabels[event.option.value.key] = event.option.value.value;
+		this.labelInput.nativeElement.value = '';
+		this.labelCtrl.setValue(null);
+
+
+		this.filterLinks();
+	}
+
+	private filterLinks(): void {
+		console.log('ss', this.selectedLabels);
+		this.filteredLinks = this.links.filter(l => {
+			let match = 0;
+			const keys = Object.keys(this.selectedLabels);
+			keys.forEach((s) => {
+				if (l.labels.includes(this.selectedLabels[s])) {
+					match++;
+				}
+			});
+			if (match === keys.length) {
+				return l;
+			}
+			console.log(l.labels);
+		});
+	}
+
+	private _filter(value: any): Label {
+		let filterValue;
+		if (value?.value) {
+			filterValue = value.value.toLowerCase();
+		} else {
+			filterValue = value.toLowerCase();
+		}
+
+		let ret: Label = {};
+
+		Object.keys(this.labels).forEach((key) => {
+			if ((this.labels[key].toLowerCase()).indexOf(filterValue) > -1) {
+				ret[key] = this.labels[key];
+			}
+		});
+
+		return ret;
 	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
 	declarations: [
@@ -10,7 +19,15 @@ import {AppComponent} from './app.component';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatAutocompleteModule,
+		MatFormFieldModule,
+		MatChipsModule,
+		MatInputModule,
+		MatIconModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
